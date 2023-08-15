5de5755cbe13b347b3da2320bf4ea2e039b4b4d6
CaioPinho9 <caiopinho9@gmail.com>
2023-08-15 19:48:06 -0300

    misc: <- to =

--- AULA 3 - Correlação Linear-20230815/ceramica.R
+++ AULA 3 - Correlação Linear-20230815/ceramica.R
@@ -1,14 +1,14 @@
 library(data.table)
-Base <- fread(input = paste0("ceramica.csv"), 
-              header = T, na.strings = "NA", 
-              data.table = FALSE, dec=",")
+Base = fread(input = paste0("ceramica.csv"),
+              header = T, na.strings = "NA",
+              data.table = FALSE, dec = ",")
 
 #Gráfico
 plot(Base)
 
 # Coeficiente de correlação
 cor(Base)
-correlacao <- cor(Base)
+correlacao = cor(Base)
 
 # Matriz do correlação
 library(ggcorrplot)
