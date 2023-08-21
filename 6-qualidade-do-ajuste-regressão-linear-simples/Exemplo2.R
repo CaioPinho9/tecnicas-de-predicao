85baf80c41c529fa08bf64ff0dee93905167c626
CaioPinho9 <caiopinho9@gmail.com>
2023-08-21 18:41:29 -0300

    rename: folders

---
+++ 6-qualidade-do-ajuste-regressão-linear-simples/Exemplo2.R
@@ -0,0 +1,16 @@
+library(data.table)
+Base <- fread(input = paste0("Exercicio.csv"), header = T, na.strings = "NA", data.table = FALSE, dec=",")
+
+#Gráfico
+
+# Coeficiente de correlação
+
+# Regressão linear
+
+# Reta de regressão no modelo
+
+# Coeficiente de determinação (R2)
+
+# Análise de resíduos
+
+# Previsão
