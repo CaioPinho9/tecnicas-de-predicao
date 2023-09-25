ea68924b4d3eeb6987c625c5e80fcdeaf722cd1b
CaioPinho9 <caiopinho9@gmail.com>
2023-09-25 18:42:17 -0300

    feat: aula14 variaveis qualitativas

---
+++ 14-variaveis-qualitativas/Sintaxe_salarios.R
@@ -0,0 +1,10 @@
+library(data.table)
+base <- fread(input = paste0("salarios.csv"), header = T, na.strings = "NA", data.table = FALSE, dec=",")
+names(base)
+
+# Modelo de Regressão Múltipla
+modelo <- lm(Salario ~ Experiencia + Gerencia + Educacional, data=base)
+summary(modelo)
+
+library(car)
+Anova(modelo)
